df4f7c8b8d4b1b28d1944a7dbcdbe7ac6407ce4c
s123dhara <spdh427@gmail.com>
2025-05-15 19:09:44 +0530

    Stop tracking 'Old Files' directory

--- Old Files/components/ui/Card.jsx
+++
@@ -1,8 +0,0 @@
-const Card = ({ children, className = "", ...props }) => {
-    return (
-        <div className={`bg-white rounded-lg shadow-md overflow-hidden ${className}`} {...props}>
-            {children}
-        </div>
-    );
-};
-
